0334b43092401529b8251c3a39a109854d6c2473
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2023-10-27 19:52:38 +0800

    second order no zeroes transfer fn is validated, okay

--- src/examples/second_order_demos.rs
+++ src/examples/second_order_demos.rs
@@ -60,7 +60,7 @@ pub(crate) fn stable_second_order_simulation(){
 
     // writer creation
 
-    let mut wtr = tf.spawn_writer("demo_".to_string()).unwrap();
+    let mut wtr = tf.spawn_writer("one_zero_two_complex_poles_demo_".to_string()).unwrap();
 
     let stuff_to_do_in_simulation_loop = move ||{
 
@@ -95,6 +95,79 @@ pub(crate) fn stable_second_order_simulation(){
         //    input_string,
         //    output_string]).unwrap();
         //wtr.flush().unwrap();
+        
+        current_simulation_time += timestep;
+    };
+
+    // need to create a pointer for the stuff_to_do_in_simulation_loop
+    // this is to enable parallelism
+    let user_task_ptr = Arc::new(Mutex::new(stuff_to_do_in_simulation_loop));
+    simulation_template(max_simulation_time, timestep, current_simulation_time,
+        user_task_ptr);
+
+
+}
+
+
+/// 
+/// This is a simulation of:
+///
+///         2.5
+/// G(s) = ---------------------------
+///         3 s^2 + 4 s + 4
+///
+///
+/// Input is in the form:
+///
+/// G(s) = 
+///
+/// a1 s^2 + b1 s + c1
+/// ------------------
+/// a2 s^2 + b2 s + c2
+///
+pub(crate) fn no_zeroes_stable_underdamped_second_order_simulation(){
+
+    use uom::si::{Quantity, ISQ, SI};
+    use uom::typenum::*;
+    // type alias called TimeSquared
+    type TimeSquared = 
+    Quantity<ISQ<Z0, Z0, P2, Z0, Z0, Z0, Z0>, SI<f64>, f64>;
+
+    let one_second = Time::new::<second>(1.0);
+
+    let a1: TimeSquared = Time::ZERO * Time::ZERO;
+    let b1: Time = Time::ZERO;
+    let c1: Ratio = Ratio::new::<ratio>(2.5);
+
+    let a2: TimeSquared =one_second * one_second* 3.0;
+    let b2: Time = Time::new::<second>(4.0);
+    let c2: Ratio = Ratio::new::<ratio>(4.0);
+    let mut current_simulation_time: Time = Time::new::<second>(0.0);
+    let max_simulation_time: Time = Time::new::<second>(30 as f64);
+    let timestep: Time = Time::new::<second>(0.1);
+
+    let mut tf = TransferFnSecondOrder::new(a1, b1, c1, a2, b2, c2).unwrap();
+    //
+    let mut user_input = Ratio::ZERO;
+
+    // writer creation
+
+    let mut wtr = tf.spawn_writer("demo_no_zeroes_".to_string()).unwrap();
+
+    let stuff_to_do_in_simulation_loop = move ||{
+
+        
+        // step up to 5 if t > 0 
+        if current_simulation_time > Time::ZERO {
+            user_input = Ratio::new::<ratio>(5.0);
+        }
+
+        let output = tf.set_user_input_and_calc(
+            user_input,current_simulation_time).unwrap();
+
+        let writer_borrow = &mut wtr;
+        tf.csv_write_values(writer_borrow, current_simulation_time, 
+            user_input, output).unwrap();
         
         current_simulation_time += timestep;
     };
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ mod examples;
 fn main() {
     println!("library_demo");
     examples::second_order_demos::stable_second_order_simulation();
+    examples::second_order_demos::no_zeroes_stable_underdamped_second_order_simulation();
 }
 
