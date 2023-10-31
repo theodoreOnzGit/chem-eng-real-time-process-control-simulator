e3b9fcbf8137d41316b06903655ba2e5ad05bd24
teddy0-mint-box <theodore_ong@berkeley.edu>
2023-10-31 13:40:32 +0800

    added default for derivative filter

--- src/lib/alpha_nightly/controllers/filtered_derivative_controller/mod.rs
+++ src/lib/alpha_nightly/controllers/filtered_derivative_controller/mod.rs
@@ -1,4 +1,7 @@
 use crate::alpha_nightly::transfer_fn_wrapper_and_enums::TransferFnFirstOrder;
+use uom::si::f64::*;
+use uom::ConstZero;
+use uom::si::time::second;
 
 /// a filtered derivative controller 
 ///
@@ -14,4 +17,25 @@ pub struct FilteredDerivativeController{
     pub transfer_fn: TransferFnFirstOrder,
 }
 
+impl Default for FilteredDerivativeController {
+    /// gives: 
+    /// G(s) = s / (0.1 s + 1)
+    fn default() -> Self {
+
+        // G(s) = (a1 s + b1)/(a2 s + b2)
+        //
+        // a1 = 1 second 
+        // b1 = 0 (ratio)
+        // a2 = 0.1 second 
+        // b2 = 0 (ratio)
+        let b1 = Ratio::ZERO;
+        let b2 = Ratio::ZERO;
+        let a1 = Time::new::<second>(1.0);
+        let a2 = Time::new::<second>(0.1);
+        let transfer_fn = TransferFnFirstOrder::new(a1, b1, a2, b2).unwrap();
+
+        return Self { transfer_fn };
+
+    }
+}
 
