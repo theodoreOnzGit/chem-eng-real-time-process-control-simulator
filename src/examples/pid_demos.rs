2c55f2f441cdcbd500ece1807b119ed71cd590fd
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2023-10-31 21:57:50 +0800

    first controller test written

--- src/examples/pid_demos.rs
+++ src/examples/pid_demos.rs
@@ -20,25 +20,65 @@ use std::thread;
 use chem_eng_real_time_process_control_simulator::alpha_nightly::prelude::*;
 use uom::ConstZero;
 use uom::si::f64::*;
+use uom::si::frequency::hertz;
 use uom::si::ratio::ratio;
-use uom::si::time::second;
+use uom::si::time::{second, millisecond};
 
 /// 
-/// This is a simulation of three step changes for an integral 
-/// controller 1/s
+/// This is a simulation of a feedback PI controller 
+/// The process gain is:
+///
+///         2.5s^2 - 0.5 s + 1
+/// G(s) = ---------------------------
+///         3 s^2 + 4 s + 4
+///
+/// we use a feedback controller with 
+/// K_c = 0.5 
+/// and K_c/tau_i = 0.3 Hertz
 pub(crate) fn proportional_integral_test(){
 
-    let one_second = Time::new::<second>(1.0);
 
-    let integral_time: Time = one_second;
-    let controller_gain = Ratio::new::<ratio>(1.0);
+    let controller_gain = Ratio::new::<ratio>(0.5);
+    let integral_time: Time = controller_gain / Frequency::new::<hertz>(0.3);
+
     let mut current_simulation_time: Time = Time::new::<second>(0.0);
     let max_simulation_time: Time = Time::new::<second>(60 as f64);
     let timestep: Time = Time::new::<second>(0.2);
 
-    let mut integral_controller: Controller = 
-        IntegralController::new(controller_gain,
-            integral_time).unwrap().into();
+    let mut pi_controller: Controller = 
+        Controller::new_pi_controller(controller_gain,
+            integral_time).unwrap();
+
+    // we also have a measurement delay of 0.0001 s 
+    // or 0.1 ms
+    let measurement_delay = Time::new::<millisecond>(0.1);
+
+    let mut measurement_delay_block: Controller = 
+    ProportionalController::new(Ratio::new::<ratio>(1.0)).unwrap().into();
+
+    measurement_delay_block.set_dead_time(measurement_delay);
+    // now for the transfer function 
+
+    use uom::si::{Quantity, ISQ, SI};
+    use uom::typenum::*;
+    // type alias called TimeSquared
+    type TimeSquared = 
+    Quantity<ISQ<Z0, Z0, P2, Z0, Z0, Z0, Z0>, SI<f64>, f64>;
+
+    let one_second = Time::new::<second>(1.0);
+
+    let a1: TimeSquared = one_second * Time::new::<second>(2.5);
+    let b1: Time = -Time::new::<second>(0.5);
+    let c1: Ratio = Ratio::new::<ratio>(1.0);
+
+    let a2: TimeSquared =one_second * one_second* 3.0;
+    let b2: Time = Time::new::<second>(4.0);
+    let c2: Ratio = Ratio::new::<ratio>(4.0);
+    let mut current_simulation_time: Time = Time::new::<second>(0.0);
+    let max_simulation_time: Time = Time::new::<second>(30 as f64);
+    let timestep: Time = Time::new::<second>(0.1);
+
+    let mut tf = TransferFnSecondOrder::new(a1, b1, c1, a2, b2, c2).unwrap();
 
     //
     // if you need to set initial values
@@ -51,40 +91,48 @@ pub(crate) fn proportional_integral_test(){
     // // the initial input and output is always zero
     // tf.set_dead_time(initial_value);
 
-    let mut user_input = Ratio::ZERO;
+    let mut user_set_point = Ratio::ZERO;
+    let mut measured_output = Ratio::ZERO;
 
     // writer creation
 
-    let mut wtr = integral_controller.spawn_writer("demo_ramp_fn".to_string()).unwrap();
+    let mut wtr = pi_controller.spawn_writer("demo_pi_controller_test".to_string()).unwrap();
 
     let stuff_to_do_in_simulation_loop = move ||{
         // for this case, I have three step functions 
         // the step functions are:
         //
-        // at 5s to 10s, input is 1 
-        // at at 10s to 15s, input is 2.5
-        // at 15s onwards, input is -1
+        // 0 to 5s, input is zero 
+        // 5s onwards, input (set point) is 5 (dimensionless)
 
         if current_simulation_time <= Time::ZERO {
             // do nothing, leave it at zero
-        } else if current_simulation_time < Time::new::<second>(5.0) {
-            user_input = Ratio::ZERO;
-        } else if current_simulation_time < Time::new::<second>(10.0) {
-            user_input = Ratio::new::<ratio>(1.0);
-        } else if current_simulation_time < Time::new::<second>(15.0) {
-            user_input = Ratio::new::<ratio>(2.5);
-        } else {
-            user_input = Ratio::new::<ratio>(-1.0);
-        }
+        } else if current_simulation_time > Time::new::<second>(5.0) {
+            user_set_point = Ratio::new::<ratio>(5.0);
+        } 
 
-        let output = integral_controller.set_user_input_and_calc(
-            user_input, current_simulation_time).unwrap();
+        // error = y_sp(t) - y(t)
+        let set_point_error = user_set_point - measured_output;
+
+        // true output
+
+        let transfer_fn_input = pi_controller.set_user_input_and_calc(
+            set_point_error, current_simulation_time).unwrap();
+
+        let tf_output = tf.set_user_input_and_calc(transfer_fn_input, 
+            current_simulation_time).unwrap();
+
+        // measured output set for next timestep
+
+        measured_output = measurement_delay_block.set_user_input_and_calc(
+            tf_output, current_simulation_time).unwrap();
+
 
         // write 
         let writer_borrow = &mut wtr;
-        integral_controller.csv_write_values(
+        pi_controller.csv_write_values(
             writer_borrow, current_simulation_time, 
-            user_input, output).unwrap();
+            user_set_point, tf_output).unwrap();
 
         current_simulation_time += timestep;
     };
