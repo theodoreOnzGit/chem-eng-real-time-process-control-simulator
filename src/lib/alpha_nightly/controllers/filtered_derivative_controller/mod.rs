36e4087f8e80cb7a159fbe546d84376273f517ea
teddy0-mint-box <theodore_ong@berkeley.edu>
2023-10-31 13:43:23 +0800

    implemented transfer fn traits for filtered derivative controller

--- src/lib/alpha_nightly/controllers/filtered_derivative_controller/mod.rs
+++ src/lib/alpha_nightly/controllers/filtered_derivative_controller/mod.rs
@@ -1,7 +1,8 @@
-use crate::alpha_nightly::transfer_fn_wrapper_and_enums::TransferFnFirstOrder;
+use crate::alpha_nightly::transfer_fn_wrapper_and_enums::{TransferFnFirstOrder, TransferFnTraits};
 use uom::si::f64::*;
 use uom::ConstZero;
 use uom::si::time::second;
+use crate::alpha_nightly::errors::ChemEngProcessControlSimulatorError;
 
 /// a filtered derivative controller 
 ///
@@ -36,6 +37,34 @@ impl Default for FilteredDerivativeController {
 
         return Self { transfer_fn };
 
+    }
+}
+
+impl TransferFnTraits for FilteredDerivativeController {
+    fn set_dead_time(&mut self, dead_time: Time) {
+        self.transfer_fn.set_dead_time(dead_time)
+    }
+
+    fn set_user_input_and_calc(&mut self, 
+        user_input: Ratio,
+        time_of_input: Time) -> Result<Ratio, 
+    ChemEngProcessControlSimulatorError> {
+        self.transfer_fn.set_user_input_and_calc(user_input, time_of_input)
+    }
+
+    fn spawn_writer(&mut self, name: String) -> Result<csv::Writer<std::fs::File>,
+    ChemEngProcessControlSimulatorError> {
+
+        self.transfer_fn.spawn_writer(name + "_filtered_derivative_controller_")
+    }
+
+    fn csv_write_values(&mut self, 
+        wtr: &mut csv::Writer<std::fs::File>,
+        time: Time,
+        input: Ratio,
+        output: Ratio) -> Result<(), 
+    ChemEngProcessControlSimulatorError> {
+        self.transfer_fn.csv_write_values(wtr, time, input, output)
     }
 }
 
